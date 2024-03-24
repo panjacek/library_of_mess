d389118fce54a71b74a5fc43d7dcfd2c08ba75a8
panjacek <panjacek@users.noreply.github.com>
2024-03-24 18:00:50 +0100

    Split db generation/tagin/pure view

--- pages/003_tagging.py
+++ pages/003_tagging.py
@@ -1,25 +1,17 @@
-import logging
-
 import streamlit as st
 
-from helpers.common import filter_db, load_db
+from helpers.common import filter_db, load_db, update_db
 
-st.set_page_config(page_title="Library of Mess", page_icon="📚", layout="wide")
-
-logger = logging.getLogger(__name__)
-
-st.title("Library of Mess")
+st.set_page_config(page_title="Library of Mess - Tagging", page_icon="📚", layout="wide")
 
 load_db()
 db = st.session_state.db
-if db is None:
-    st.warning("No videos found, check your database")
-    st.stop()
-
 filtered_db = filter_db()
 
-st.write(filtered_db)
-show_video = st.selectbox("Show Video", sorted(filtered_db["path"]), index=None)
+edited_db = st.data_editor(filtered_db)
+if st.button("Save Updates?"):
+    update_db(edited_db)
+show_video = st.selectbox("Show Video", sorted(filtered_db["path"]))
 if show_video:
     # FIXME: HEVC H265 is not supported :/
     # TODO: check if video is HEVC
