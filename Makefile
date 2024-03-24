69d41779dc1f915136d38befdbaa8a4048f1d52b
panjacek <panjacek@users.noreply.github.com>
2024-03-24 16:09:29 +0100

    Poor mans tags work

--- Makefile
+++ Makefile
@@ -1,16 +1,26 @@
+SHOW_LOGS ?= 0
+
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
 build:  ## build image
 	docker compose build
-
 
 shell:  ## open shell
 	docker compose run --rm -it library_of_mess /bin/bash
 
 up:  ## start containers
 	docker compose up -d
+ifeq ("$(SHOW_LOGS)", "1")
+	docker compose logs -f
+endif
 
-down:  ## stop containers
+stop:  ## stop containers
 	docker compose down
 
 format:  ## format code
 	isort .
 	black .
+
+check_pip:
+	docker compose run --rm -it library_of_mess /bin/bash -c "pip list -u && pip list --outdated" 
--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+import logging
 from pathlib import Path
 
+import mutagen
 import pandas as pd
 import streamlit as st
 
 st.set_page_config(page_title="Library of Mess", page_icon="📚", layout="wide")
 
+logger = logging.getLogger(__name__)
+
 LIBRARY_PATH = "/app/library"
 DB_PATH = "/app/library.db"
-DB_COLUMNS = ["path", "name", "year", "tags"]
+DB_COLUMNS = ["path", "name", "year", "tags", "length", "bike", "hyperlapse"]
+# these are only generated, do not edit them
+DB_COLUMNS_RO = ["path", "name", "length"]
+MIME_TO_CHECK = ["video/mp4"]
 
 
 def parse_video_paths(list_of_videos) -> pd.DataFrame:
@@ -17,8 +24,20 @@ def parse_video_paths(list_of_videos) -> pd.DataFrame:
         video_dict["path"].append(str(video_path))
         video_dict["name"].append(video_path.stem)
         video_dict["year"].append("")
-        video_dict["tags"].append([])
 
+        # parse metadata is super slow..
+        mutagen_info = mutagen.File(video_path)
+        logger.debug(f"Len={mutagen_info.info.length}s, {video_path}")
+
+        video_dict["length"].append(round(mutagen_info.info.length, 0))
+
+        # generic tags
+        video_dict["tags"].append("")
+        # more specific importat tags
+        video_dict["bike"].append(False)
+        video_dict["hyperlapse"].append(False)
+
+    st.write(f"Found {len(video_dict['path'])} videos in library folders")
     return pd.DataFrame(data=video_dict)
 
 
@@ -33,13 +52,22 @@ def refresh_db(db_df):
             st.write(video_files)
     else:
         video_files = Path(LIBRARY_PATH).glob("**/*.MP4")
+
     video_df = parse_video_paths(video_files)
-    db_df = pd.concat([db_df, video_df], ignore_index=True).drop_duplicates(subset="path")
+    # compare DBs
+    for index, row in video_df.iterrows():
+        # add new video if not present if main db
+        if row["path"] not in db_df["path"].values:
+            db_df.loc[len(db_df.index)] = row
+        else:
+            pass
+            # update video if present
+            # Insert new columns here
+            # db_df.loc[db_df["path"] == row["path"], "hyperlapse"] = row["hyperlapse"]
+            # TODO: compare metadata and update if new tags or something was added to file
     return db_df
 
-
 st.title("Library of Mess")
-
 
 # Init DB if not present
 db = None
@@ -53,25 +81,48 @@ if st.button("Refresh DB"):
     db = refresh_db(db)
     db.to_parquet(DB_PATH)
 
-name_filter = st.text_input("Filter by name", "2024")
-db = db[db["path"].str.contains(name_filter)]
-if st.checkbox("Show DB"):
+if st.checkbox("Show raw DB"):
     st.write(db)
 
+name_filter = st.text_input("Filter by name", "")
+tags_filter = st.text_input("Filter by tags", "")
+filtered_db = db[db["path"].str.contains(name_filter)]
+if tags_filter:
+    filtered_db = filtered_db[filtered_db["tags"].str.contains(tags_filter)]
+
+if st.checkbox("Hide nonbikes"):
+    filtered_db = filtered_db[filtered_db["bike"] == True]  # noqa: E712
+
+if filtered_db.empty:
+    st.write("No videos found")
+    st.stop()
+
+# Cut long/short
+len_filter = st.slider(
+    "Length",
+    min_value=filtered_db["length"].min(),
+    max_value=filtered_db["length"].max(),
+    value=(filtered_db["length"].min(), filtered_db["length"].max()),
+)
+filtered_db = filtered_db[
+    (filtered_db["length"] >= len_filter[0]) & (filtered_db["length"] <= len_filter[1])
+]
+
 if st.checkbox("Show Video"):
-    show_video = st.selectbox("Show Video", db["path"])
+    edited_db = st.data_editor(filtered_db)
+    if st.button("Save Updates?"):
+        for index, row in edited_db.iterrows():
+            db.loc[db["path"] == row["path"], "year"] = row["year"]
+            db.loc[db["path"] == row["path"], "tags"] = row["tags"]
+            db.loc[db["path"] == row["path"], "bike"] = row["bike"]
+            db.loc[db["path"] == row["path"], "hyperlapse"] = row["hyperlapse"]
+        db.to_parquet(DB_PATH)
+    show_video = st.selectbox("Show Video", sorted(filtered_db["path"]))
     if show_video:
+        # FIXME: HEVC H265 is not supported :/
+        # TODO: check if video is HEVC
+        # TODO: short or thumbnail:
+        #       there seems to be play limit but still loads full video
+        #       see https://github.com/streamlit/streamlit/issues/946
         with open(show_video, "rb") as video_file:
-            st.video(video_file.read())
-
-# TODO: specific year/folder per tab
-if False:
-    st.session_state["tabs"] = []
-    for folder in chosen_folders:
-        if folder.is_dir():
-            st.write(folder.basename)
-            st.write(folder.glob("*.mp4"))
-            st.session_state["tabs"].append(f"{folder.basename}")
-
-    if st.button("Show"):
-        tabs = st.tabs(st.session_state["tabs"])
+            st.video(video_file)
